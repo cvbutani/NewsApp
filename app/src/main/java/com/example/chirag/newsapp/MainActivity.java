d568beb6ae527d85b6fcdc11fe21a7202d4e255a
cvbutani <chirag.butani1@gmail.com>
2018-07-01 18:54:06 -0400

    minor fix

--- app/src/main/java/com/example/chirag/newsapp/MainActivity.java
+++ app/src/main/java/com/example/chirag/newsapp/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.chirag.newsapp;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+
 import android.net.Uri;
+
 import android.os.Bundle;
+
 import android.preference.PreferenceManager;
+
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,8 +18,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.util.Log;
+
 import android.view.Menu;
 import android.view.MenuItem;
+
 import android.widget.SearchView;
 
 import com.example.chirag.newsapp.Constants.ApiRequestConstant;
